fd7b27fd9de5bb165717db073f137714ee6a4822
Vlad104 <vlad104da@gmail.com>
2019-02-22 01:39:19 +0300

    Версия перед тестированием

--- MC3/MC3/specordermodel.cpp
+++ MC3/MC3/specordermodel.cpp
@@ -4,7 +4,8 @@ SpecOrderModel::SpecOrderModel(OrderModel& order)
 {
 
     //QStringList horizontalHeader {"Дозатор", "Объём", "Подача", "A:B", "Реверс"};
-    QStringList horizontalHeader {"Дозатор", "Объём", "Подача", "A:B"};
+    //QStringList horizontalHeader {"Дозатор", "Объём", "Подача", "A:B"};
+    QStringList horizontalHeader {"Дозатор", "Объём", "Подача", "A:B", "Время"};
     this->setHorizontalHeaderLabels(horizontalHeader);
 
     for (auto& data_model : order) {
@@ -26,12 +27,11 @@ SpecOrderModel::SpecOrderModel(OrderModel& order)
         QString ratio = QString::number(data_model.get_ratio_A()) + " : " + QString::number(data_model.get_ratio_B());
         item_->setText(ratio);
         row.append(item_);
-        /*
         item_ = new QStandardItem();
         item_->setTextAlignment(Qt::AlignCenter);
-        item_->setText(QString::number(data_model.get_reverse()));
+        item_->setText("-");
+        //item_->setText(QString::number(data_model.get_reverse()));
         row.append(item_);
-        */
         this->appendRow(row);
     }
 }
